d1d50fc2927992c4e525a46e17692c2345919a23
grealisa <grealisa@onid.oregonstate.edu>
2015-05-03 15:54:53 -0700

    Debugged check functions in multtable.php

--- src/multtable.php
+++ src/multtable.php
@@ -9,19 +9,19 @@ ini_set('display_errors','On');
     <title>CS290 Assignment 4 part 1</title>
   </head>
   <body>
-    <?php 
+    <?php
       $eCheck=false; //if errors are found, change to true
-      if(!eCheck) { //if no errors so far, check existence of parameters
-        checkExist($_GET);
+      if(!$eCheck) { //if no errors so far, check existence of parameters
+        checkExist($eCheck);
       }
-      if(!eCheck) { //if params exist, check if they are integers
-        checkInt($_GET);
+      if(!$eCheck) { //if params exist, check if they are integers
+        checkInt($eCheck);
       }
-      if(!eCheck) { //if params exist and are integers, check min<=max
-        checkMinMax($_GET);
+      if(!$eCheck) { //if params exist and are integers, check min<=max
+        checkMinMax($eCheck);
       }
       
-      function checkExist($_GET) {
+      function checkExist(&$eCheck) {
         if($_GET['min-multiplicand'] === "") { //if min-multiplicand doesn't exist, print error message
           $eCheck=true; //an error occurred, so set eCheck to true
           echo "<p>Missing parameter min-multiplicand.";
@@ -40,26 +40,26 @@ ini_set('display_errors','On');
         }
       }
       
-      function checkInt($_GET) { //is_int function came from PHP manual: http://php.net/manual/en/function.is-int.php
-        if(!(is_int($_GET['min-multiplicand']))) { //if min-multiplicand isn't integer, print error message
+      function checkInt(&$eCheck) { //is_numeric function came from PHP manual: http://php.net/manual/en/function.is-numeric.php
+        if(!(is_numeric($_GET['min-multiplicand']))) { //if min-multiplicand isn't integer, print error message
           $eCheck=true; //an error occurred, so set eCheck to true
           echo "<p>min-multiplicand must be an integer.";
         }
-        if(!(is_int($_GET['max-multiplicand']))) { //if max-multiplicand isn't integer, print error message
+        if(!(is_numeric($_GET['max-multiplicand']))) { //if max-multiplicand isn't integer, print error message
           $eCheck=true; //an error occurred, so set eCheck to true
           echo "<p>max-multiplicand must be an integer.";
         }
-        if(!(is_int($_GET['min-multiplier']))) { //if min-multiplier isn't integer, print error message
+        if(!(is_numeric($_GET['min-multiplier']))) { //if min-multiplier isn't integer, print error message
           $eCheck=true; //an error occurred, so set eCheck to true
           echo "<p>min-multiplier must be an integer.";
         }
-        if(!(is_int($_GET['max-multiplier']))) { //if max-multiplier isn't integer, print error message
+        if(!(is_numeric($_GET['max-multiplier']))) { //if max-multiplier isn't integer, print error message
           $eCheck=true; //an error occurred, so set eCheck to true
           echo "<p>max-multiplier must be an integer.";
         }
       }
       
-      function checkMinMax($_GET) {
+      function checkMinMax(&$eCheck) {
         if($_GET['min-multiplicand']>$_GET['max-multiplicand']) {
           //if min-multiplicand > max-multiplicand print error message
           $eCheck=true; //an error occurred, so set eCheck to true
@@ -71,6 +71,6 @@ ini_set('display_errors','On');
           echo "<p>Minimum multiplier larger than maximum.";
         }
       }
-    ?>
+?>
   </body>
 </html>
