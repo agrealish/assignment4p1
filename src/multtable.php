76674b4f7997708a2dd18b640b1fb9d8c3bbb6fe
grealisa <grealisa@onid.oregonstate.edu>
2015-05-03 13:19:15 -0700

    Added form to multtable.php and tested

---
+++ src/multtable.php
@@ -0,0 +1,22 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>CS290 Assignment 4 part 1</title>
+  </head>
+  <body>
+    <form action="multtable.php" method="get">
+      Enter Parameters:
+      <br><br>
+      Minimum multiplicand: <input type="number" name="min-multiplicand">
+      <br>
+      Maximum multiplicand: <input type="number" name="max-multiplicand">
+      <br>
+      Minimum multiplier: <input type="number" name="min-multiplier">
+      <br>
+      Maximum multiplier: <input type="number" name="max-multiplier">
+      <br>
+      <input type="submit">
+      <br>
+    </form>
+  </body>
+</html>
